d12e133a3407c80c905b4dc33a2631c98cd0f55b
dogpunk <przezdziecki.a@gmail.com>
2017-08-29 01:45:30 +0200

    hello world

---
+++ public/server.js
@@ -0,0 +1,22 @@
+import app from './../server';
+import config from './../constants/server.json';
+// import { connectDatabase } from './../server/db';
+
+const port = process.env.PORT || config.DEFAULT_PORT;
+
+(async () => {
+  try {
+    // const info = await connectDatabase(development);
+    // console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+  } catch (error) {
+    // console.error('Unable to connect to database');
+  }
+
+  try {
+    await app.listen(port);
+    console.log('Hello World!');
+    console.log(`Server started on port ${port}`);
+  } catch (error) {
+    console.log(error);
+  }
+})();
--- server/index.js
+++ server/index.js
@@ -1,18 +1,35 @@
 import Koa from 'koa';
-import middleware from './middleware';
-import config from './../constants/server.json';
+
+import compose from 'koa-compose';
+import convert from 'koa-convert';
+import logger from 'koa-logger';
+import helmet from 'koa-helmet';
+import cors from 'koa-cors';
+import bodyParser from 'koa-bodyparser';
+import session from 'koa-session';
 
 const app = new Koa();
-const PORT = process.env.PORT ? process.env.PORT : config.DEFAULT_PORT;
 
-app.use(middleware());
+const SESSION_CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 86400000,
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. **/
+};
 
-app.use((ctx) => {
-  ctx.body = 'Hello Koa';
-});
-
-app.listen(PORT, () => {
-
-});
+app.use(
+  compose([
+    logger(),
+    helmet(),
+    convert(cors()),
+    convert(bodyParser()),
+    session(SESSION_CONFIG, app),
+  ]),
+);
 
 export default app;
